fc2690eb042b1358b2db898963035a58ba3957cb
Tanti Amelia Sopya <ameliatanty1798@upi.edu>
2020-07-24 11:28:53 +0700

    merubah index.php

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
 <body>
   <h2>Hello World!</h2>
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat pariatur, voluptatibus deleniti quam adipisci debitis nostrum fuga, architecto necessitatibus itaque quibusdam. Neque qui quo fugit pariatur quas repellendus explicabo.</p>
+  <li>Hi</li>
+  <li>HBD</li>
 </body>
+
 
 </html>
