1127c766e107b1da235492def47befa043ece363
Tanti Amelia Sopya <ameliatanty1798@upi.edu>
2020-07-24 11:24:29 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
   <h2>Hello World!</h2>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat pariatur, voluptatibus deleniti quam adipisci debitis nostrum fuga, architecto necessitatibus itaque quibusdam. Neque qui quo fugit pariatur quas repellendus explicabo.</p>
 </body>
 
 </html>
